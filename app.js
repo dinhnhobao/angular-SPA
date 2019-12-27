7a90c0fb397a92d9a6f867ab99b11d04d7992ad2
Dinh Nho Bao <nhobao2000@gmail.com>
2019-12-27 15:19:56 +0800

    features section completed, changed services info

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(function ($routeProvider) {
+app.config(function($routeProvider) {
     $routeProvider
-
         .when('/', {
             templateUrl: 'home_template',
             controller: 'HomeController'
         })
 
-        .when('/footer', {
-            templateUrl: 'footer_template',
-            controller: 'FooterController'
-        })
+    .when('/footer', {
+        templateUrl: 'footer_template',
+        controller: 'FooterController'
+    })
 
-        .when('/review', {
-            templateUrl: 'review_template',
-            controller: 'ReviewController'
-        })
+    .when('/review', {
+        templateUrl: 'review_template',
+        controller: 'ReviewController'
+    })
 
-        .when('/faq', {
-            templateUrl: 'faq_template',
-            controller: 'FAQController'
-        })
+    .when('/faq', {
+        templateUrl: 'faq_template',
+        controller: 'FAQController'
+    })
 
-        .when('/services', {
+    .when('/services', {
             templateUrl: 'service_template',
             controller: 'ServiceController'
         })
         .otherwise({ redirectTo: '/' });
 });
 
-app.controller('HomeController', function ($scope) {
-});
+app.controller('HomeController', function($scope) {});
 
-app.controller('FooterController', function ($scope) {
-});
+app.controller('FooterController', function($scope) {});
 
-app.controller('ReviewController', function ($scope) {
-});
+app.controller('ReviewController', function($scope) {});
 
-app.controller('FAQController', function ($scope) {
-});
+app.controller('FAQController', function($scope) {});
 
-app.controller('ServiceController', function ($scope) {
-});
+app.controller('ServiceController', function($scope) {});
